7243f00c8d8155f61ba2cbf158bfa79a7eb9cd8e
Alexandru Rosianu <me@aluxian.com>
2020-06-09 13:49:13 +0100

    Update __pet_select.fish

--- functions/__pet_select.fish
+++ functions/__pet_select.fish
@@ -1,4 +1,4 @@
-function pet_select --description 'Search pet (the snippet manager) for a query and replace the command line with the result'
+function __pet_select --description 'Search pet (the snippet manager) for a query and replace the command line with the result'
     set -l query (commandline)
     pet search --query "$query" $argv | read cmd
     and commandline -- $cmd
